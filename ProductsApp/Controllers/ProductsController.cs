dacc7511ebf981a0989bea7c4b53df0e16f639c1
dadcovn <vnguyen@dadco.com>
2015-06-08 10:10:18 -0700

    initial update

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        Product[] products1 = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Veggie", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "QueenElsa", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Surface3", Category = "Hardware", Price = 16.99M } 
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        //public IEnumerable<Product> Get()
+        //{
+        //    return products1;
+        //}
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
